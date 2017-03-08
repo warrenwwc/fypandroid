9ee7e30d44316e60a229050312f5c577300822b4
42480 <42480>
2017-03-08 19:01:57 +0800

    nfc update

---
+++ app/src/main/java/is/fyp/NFCDisplayActivity.java
@@ -0,0 +1,38 @@
+package is.fyp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.NfcAdapter;
+import android.os.Parcelable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class NFCDisplayActivity extends Activity {
+
+    private TextView mTextView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nfc_display);
+        mTextView = (TextView) findViewById(R.id.text_view);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Intent intent = getIntent();
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intent.getAction())) {
+            Parcelable[] rawMessages = intent.getParcelableArrayExtra(
+                    NfcAdapter.EXTRA_NDEF_MESSAGES);
+
+            NdefMessage message = (NdefMessage) rawMessages[0]; // only one message transferred
+            mTextView.setText(new String(message.getRecords()[0].getPayload()));
+
+        } else
+            mTextView.setText("Waiting for NDEF Message");
+
+    }
+}
