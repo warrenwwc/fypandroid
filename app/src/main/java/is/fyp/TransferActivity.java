40769a3f3aabc7654c0def2849685788980f9b2b
jl9404 <jl9404@hotmail.com>
2017-04-08 06:36:05 +0800

    fucking qrcode generator

--- app/src/main/java/is/fyp/TransferActivity.java
+++ app/src/main/java/is/fyp/TransferActivity.java
@@ -5,14 +5,30 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
 import android.nfc.NfcEvent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+import is.fyp.tasks.EncodeBarcodeTask;
 
 public class TransferActivity extends Activity implements NfcAdapter.CreateNdefMessageCallback {
 
@@ -33,14 +49,22 @@ public class TransferActivity extends Activity implements NfcAdapter.CreateNdefM
         NfcAdapter mAdapter = NfcAdapter.getDefaultAdapter(this);
         if (mAdapter == null) {
             Toast.makeText(this, "Sorry this device does not have NFC.", Toast.LENGTH_LONG).show();
-            return;
+        }else{
+            if (!mAdapter.isEnabled()) {
+                Toast.makeText(this, "Please enable NFC via Settings.", Toast.LENGTH_LONG).show();
+            }
+
+            mAdapter.setNdefPushMessageCallback(this, this);
         }
 
-        if (!mAdapter.isEnabled()) {
-            Toast.makeText(this, "Please enable NFC via Settings.", Toast.LENGTH_LONG).show();
-        }
-
-        mAdapter.setNdefPushMessageCallback(this, this);
+        new EncodeBarcodeTask(){
+            protected void onPostExecute(Bitmap result) {
+                if (result != null) {
+                    ImageView myImage = (ImageView) findViewById(R.id.imageView5);
+                    myImage.setImageBitmap(result);
+                }
+            }
+        }.execute(new String(this.transData(getIntent().getStringExtra("amount"))));
 
     }
 
---
+++ app/src/main/java/is/fyp/tasks/EncodeBarcodeTask.java
@@ -0,0 +1,54 @@
+package is.fyp.tasks;
+
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.os.AsyncTask;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+/**
+ * Created by Jason on 8/4/2017.
+ */
+
+public class EncodeBarcodeTask extends AsyncTask<String, Void, Bitmap> {
+    @Override
+    protected Bitmap doInBackground(String ...params) {
+        int width = 500;
+        int height = 500;
+        String value = params[0];
+        BitMatrix bitMatrix;
+        Map<EncodeHintType, Object> hints = new EnumMap<EncodeHintType, Object>(EncodeHintType.class);
+        hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+        hints.put(EncodeHintType.MARGIN, 1);
+        try {
+            hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
+            bitMatrix = new MultiFormatWriter().encode(
+                    value,
+                    BarcodeFormat.DATA_MATRIX.QR_CODE,
+                    width, height, hints
+            );
+
+        } catch (IllegalArgumentException | WriterException e) {
+            return null;
+        }
+
+        Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        bitmap.setHasAlpha(true);
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                bitmap.setPixel(x, y, bitMatrix.get(x, y) ? Color.BLACK : Color.TRANSPARENT);
+            }
+        }
+
+        return bitmap;
+    }
+}
